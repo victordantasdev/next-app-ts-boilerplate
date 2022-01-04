a1e37b6c07b3608d2a4bf04722201d4d120a2227
victordantasdev <victordantasdeveloper@gmail.com>
2022-01-04 10:25:13 -0300

    feat: themeSwitcher created

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    font-family: Ubuntu, sans-serif;
+    font-weight: 400;
+  }
+
+  ${normalize}
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    min-height: 100vh;
+    width: 100%;
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: ${(props) => props.theme.background};
+    color: ${(props) => props.theme.color};
+    transition: all 0.2s ease-in-out;
+  }
+
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
---
+++ src/styles/themes.ts
@@ -0,0 +1,23 @@
+export const dark = {
+  title: 'dark',
+
+  primary: '#bb86fc',
+  primaryVariant: '#3700b3',
+  secondary: '#03dac6',
+  secondaryVariant: '#018786',
+  background: '#121212',
+  color: '#fff',
+  error: '#cf6679',
+};
+
+export const light = {
+  title: 'light',
+
+  primary: '#6200ee',
+  primaryVariant: '#3700b3',
+  secondary: '#03dac6',
+  secondaryVariant: '#018786',
+  background: '#fff',
+  color: '#121212',
+  error: '#b00020',
+};
